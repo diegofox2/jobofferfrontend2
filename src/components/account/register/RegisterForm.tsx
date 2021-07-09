34e46709d0b43fc39988cd8716fac47e10c2c5e2
Diego Camacho <dcamacho31@hotmail.com>
2021-07-09 11:47:22 -0300

    Adding React-Forms, keep working

--- src/components/account/register/RegisterForm.tsx
+++ src/components/account/register/RegisterForm.tsx
@@ -1,46 +1,89 @@
-import React from 'react'
-import { useForm } from "react-hook-form"
-import { Form, Button } from 'react-bootstrap'
+import React, { useEffect } from "react";
+import { useForm } from "react-hook-form";
+import { Form, Button, Alert } from "react-bootstrap";
 
 type FormData = {
+  email?: string;
+  password?: string;
+  repeatedPassword?: string;
+  isRecruiter?: boolean;
+};
 
-    email: string,
-    password: string,
-    repeatedPassword: string,
-    isRecruiter: boolean
+export default function RegisterForm(props: FormData) {
+  const { register, handleSubmit, watch } = useForm<FormData>();
 
-}
+  const emailWatch = watch("email");
+  const passwordWatch = watch("password");
+  const repeatedPasswordWatch = watch("repeatedPassword");
 
-export default function RegisterForm() {
+  const [isReadyToSubmit, setIsReadyToSubmit] = React.useState<boolean>(false);
+  const [isFormInvalid, setIsFormInvalid] = React.useState<boolean>();
 
-    const { register, handleSubmit } = useForm<FormData>();
+  useEffect(() => {
+    emailWatch && passwordWatch && repeatedPasswordWatch
+      ? setIsReadyToSubmit(true)
+      : setIsReadyToSubmit(false);
+  }, [emailWatch, passwordWatch, repeatedPasswordWatch]);
 
-    const [isReadyToSubmit, setIsReadyToSubmit] = React.useState()
+  useEffect(() => {
+    isReadyToSubmit && passwordWatch !== repeatedPasswordWatch
+      ? setIsFormInvalid(true)
+      : setIsFormInvalid(false);
+  }, [isReadyToSubmit, passwordWatch, repeatedPasswordWatch]);
 
-    const onSubmit = handleSubmit((data: any) => console.log(data));
+  const onSubmit = (data: FormData) => console.log(data);
 
-    return (
-        <>
-            <Form onSubmit={onSubmit}>
-                <Form.Group controlId="emailInput">
-                    <Form.Label>Email</Form.Label>
-                    <Form.Control type="email" placeholder="Ingrese su email" {...register("email")} />
-                </Form.Group>
-                <Form.Group controlId="passwordInput">
-                    <Form.Label>Password</Form.Label>
-                    <Form.Control type="password" placeholder="Password"  {...register("password")} />
-                </Form.Group>
-                <Form.Group controlId="repeatPasswordInput">
-                    <Form.Label>Repita su password</Form.Label>
-                    <Form.Control type="password" placeholder="Password"  {...register("repeatedPassword")} />
-                </Form.Group>
-                <Form.Group controlId="isRecruiterCheck">
-                    <Form.Check type="checkbox" label="Soy recruiter" {...register("isRecruiter")} />
-                </Form.Group>
-                <Button variant={isReadyToSubmit ? "primary" : "secondary"} type="submit" disabled={!isReadyToSubmit} >
-                    Ingresar
-                </Button>
-            </Form>
-        </>
-    )
+  const errorMessage = isFormInvalid && (
+    <Alert variant="danger">Las contraseñas no coinciden</Alert>
+  );
+
+  return (
+    <>
+      {errorMessage}
+      <Form onSubmit={handleSubmit(onSubmit)}>
+        <Form.Group controlId="emailInput">
+          <Form.Label>Email</Form.Label>
+          <Form.Control
+            type="email"
+            placeholder="Ingrese su email"
+            {...register("email")}
+            defaultValue={props.email}
+          />
+        </Form.Group>
+        <Form.Group controlId="passwordInput">
+          <Form.Label>Password</Form.Label>
+          <Form.Control
+            type="password"
+            placeholder="Password"
+            {...register("password")}
+            defaultValue={props.password}
+          />
+        </Form.Group>
+        <Form.Group controlId="repeatPasswordInput">
+          <Form.Label>Repita su password</Form.Label>
+          <Form.Control
+            type="password"
+            placeholder="Password"
+            {...register("repeatedPassword")}
+            defaultValue={props.repeatedPassword}
+          />
+        </Form.Group>
+        <Form.Group controlId="isRecruiterCheck">
+          <Form.Check
+            type="checkbox"
+            label="Soy recruiter"
+            {...register("isRecruiter")}
+            checked={props.isRecruiter ? props.isRecruiter : false}
+          />
+        </Form.Group>
+        <Button
+          variant={isReadyToSubmit ? "primary" : "secondary"}
+          type="submit"
+          disabled={!isReadyToSubmit}
+        >
+          Ingresar
+        </Button>
+      </Form>
+    </>
+  );
 }
