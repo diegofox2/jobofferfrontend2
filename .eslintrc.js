2095d61a39f2a0474dc34b7e0f783d5b596a7a9d
Diego Camacho <dcamacho31@hotmail.com>
2021-08-26 17:15:22 -0300

    Updating packages, adding eslint file and rules and configuring prettier in vscode

---
+++ .eslintrc.js
@@ -0,0 +1,42 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    jest: true
+  },
+  extends: [
+    'plugin:react/recommended',
+    'plugin:jest-dom/recommended',
+    'standard'
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true
+    },
+    ecmaVersion: 12,
+    sourceType: 'module'
+  },
+  plugins: [
+    'react',
+    '@typescript-eslint',
+    'jest-dom',
+    'testing-library',
+  ],
+  rules: {
+    'testing-library/await-async-query': 'error',
+    'testing-library/no-await-sync-query': 'error',
+    'testing-library/no-debug': 'warn',
+    'testing-library/no-dom-import': 'off',
+    'jest-dom/prefer-checked': 'error',
+    'jest-dom/prefer-enabled-disabled': 'error',
+    'jest-dom/prefer-required': 'error',
+    'jest-dom/prefer-to-have-attribute': 'error',
+    semi: 0,
+    'no-use-before-define': 'off',
+    '@typescript-eslint/no-use-before-define': ['error'],
+    'react/react-in-jsx-scope': 0,
+    'react/jsx-uses-react': 0,
+    'space-before-function-paren': { named: 'never' }
+  }
+}
